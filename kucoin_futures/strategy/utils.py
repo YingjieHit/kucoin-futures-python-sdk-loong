1725f2c084f91f0cd2b97cd1f9d1e224eec4c78f
YingjieHit <258349394@qq.com>
2024-06-26 13:16:25 +0800

    1

--- kucoin_futures/strategy/utils.py
+++ kucoin_futures/strategy/utils.py
@@ -21,6 +21,37 @@ class Utils(object):
             ask_size=float(ticker_data.get("bestAskSize", 0)),  # 默认值0，转换为float
             ts=ticker_data.get("time", 0)  # 提供默认值为0
         )
+    
+    # TODO: 该方法为现货相关，只是暂时写在这里，后期需考虑合理的现货与合约结合的架构
+    @staticmethod
+    def spot_dict_2_order(order_data: dict) -> Order:
+        """
+        将spot的order数据的字典转换为Order对象，使用字典的get方法来避免KeyError。
+        """
+        order = Order(
+            symbol=order_data.get('symbol', ''),  # "symbol": "XBTUSDM"
+            side=order_data.get('side', ''),  # 訂單方向，買或賣 "buy"  "sell"
+            order_id=order_data.get('orderId', ''),  # "orderId": "5cdfc138b21023a909e5ad55", 訂單號
+            type=order_data.get('type', ''),  # " 消息類型 "open", "match", "filled", "canceled", "update"
+            fee_type=order_data.get('feeType', ''),  # 費用類型，當type = match才包含此字段，取值列表: "takerFee", "makerFee"
+            status=order_data.get('status', ''),  # 訂單狀態: "match", "open", "done"
+            match_size=float(order_data.get('matchSize', 0)),  # "matchSize": 成交數量 (當類型爲"match"時包含此字段)
+            match_price=float(order_data.get('matchPrice', 0)),  # 成交價格 (當類型爲"match"時包含此字段)
+            order_type=order_data.get('orderType', ''),  # 訂單類型, "market"表示市價單", "limit"表示限價單
+            price=float(order_data.get('price', 0)),  # "price": "3600",  //訂單價格
+            size=float(order_data.get('size', 0)),  # "size": "20000",  //訂單數量
+            remain_size=float(order_data.get('remainSize', 0)),  # "remainSize": "20001",  //訂單剩餘可用於交易的數量
+            fill_size=float(order_data.get('filledSize', 0)),  # "filledSize":"20000",  //訂單已成交的數量
+            canceled_size=float(order_data.get('canceledSize', 0)),  # "canceledSize": "0", update消息中，訂單減少的數量  注意：现货无该字段
+            trade_id=order_data.get('tradeId', ''),  # "tradeId": "5ce24c16b210233c36eexxxx", 交易號(當類型爲"match"時包含此字段)
+            client_oid=order_data.get('clientOid', ''),  # "clientOid": "5ce24c16b210233c36ee321d", //用戶自定義ID
+            order_time=order_data.get('orderTime', 0),  # "orderTime": 1545914149935808589,  // 下單時間(trade模塊生成)
+            old_size=float(order_data.get('oldSize', 0)),  # "oldSize ": "15000", // 更新前的數量(當類型爲"update"時包含此字段)
+            liquidity=order_data.get('liquidity', ''),  # "liquidity": "maker" "taker"
+            ts=order_data.get('ts', 0)  # "ts": 1545914149935808589 // 時間戳（撮合時間）单位是ns
+        )
+        order.fill_size = order.size - order.remain_size
+        return order
 
     @staticmethod
     def dict_2_ticker(ticker_data: dict) -> Ticker:
