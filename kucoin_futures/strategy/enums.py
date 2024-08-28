04d0d8da5fa22597c868a02e383cd67b65cfcc2b
YingjieHit <258349394@qq.com>
2024-08-28 18:22:46 +0800

    1

--- kucoin_futures/strategy/enums.py
+++ kucoin_futures/strategy/enums.py
@@ -2,7 +2,7 @@ class Subject:
     """订阅到的主题"""
     tickerV2 = 'tickerV2'
     symbolOrderChange = 'symbolOrderChange'
-    candleStick = 'candleStick'  # K线
+    candleStick = 'candle.stick'  # K线
     level2 = 'level2'  # level2数据
 
     
