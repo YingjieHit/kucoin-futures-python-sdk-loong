4c02379034d54bd1fa04d72b728a6f34f47bd9bd
YingjieHit <258349394@qq.com>
2024-10-26 20:29:51 +0800

    111

--- kucoin_futures/strategy/kline_data_loader.py
+++ kucoin_futures/strategy/kline_data_loader.py
@@ -3,6 +3,7 @@ from kucoin_futures.strategy.time_utils import time_utils
 from kucoin_futures.strategy.object import Bar
 from kucoin_futures.common.const import KC_TO_BN_SYMBOL, KC_TO_BN_FREQUENCY
 from binance.cm_futures import CMFutures
+from ccxt import binance
 
 
 class KlineDataLoader(object):
@@ -10,6 +11,7 @@ class KlineDataLoader(object):
     def __init__(self):
         self.market = Market()
         self.bn_cm_futures = CMFutures()
+        self.binance = binance()
 
     def get_last_n_bars(self, symbol: str, freq: str, ts: int, n: int) -> list[Bar]:
         # ts兼容s和ms
@@ -69,6 +71,53 @@ class KlineDataLoader(object):
                 startTime=start_ts,
                 endTime=end_ts,
                 limit= 1500,  # TODO: 这里可以优化
+            )
+            if len(sub_kline_data) == 0:
+                break
+            kline_data.extend(sub_kline_data)
+            start_ts = sub_kline_data[-1][0] + 1000
+            end_ts = ts if start_ts + max_ts >= ts else start_ts + max_ts
+
+        kline_data = kline_data[-n:]
+        bars = []
+        for kline in kline_data:
+            bars.append(
+                Bar(
+                    symbol=symbol,
+                    ts=kline[0] // 1000,
+                    open=float(kline[1]),
+                    close=float(kline[4]),
+                    high=float(kline[2]),
+                    low=float(kline[3]),
+                    volume=int(kline[5]),
+                    turnover=float(kline[7])
+                )
+            )
+
+        return bars
+
+    def get_bn_last_n_bars_from_ccxt(self, symbol: str, freq: str, ts: int, n: int) -> list[Bar]:
+        # 获取交易所对象
+        # 接口ts要求是毫秒
+        if time_utils.get_ts_unit(ts) == 's':
+            ts = ts * 1000
+        if time_utils.get_ts_unit(ts) != 'ms':
+            raise ValueError("begin_ts and end_ts should be ms")
+        # end_ts = ts
+        start_ts = ts - time_utils.calc_seconds_by_freq_count(freq, n) * 1000
+        max_ts = time_utils.calc_seconds_by_freq_count(freq, 1499) * 1000
+        end_ts = ts if start_ts + max_ts >= ts else start_ts + max_ts
+
+        kline_data = []
+        while len(kline_data) < n:
+            sub_kline_data = self.binance.fetch_ohlcv(
+                symbol=symbol,
+                timeframe=freq,
+                since=start_ts,
+                limit= 1500,  # TODO: 这里可以优化
+                params={
+                    'endTime': end_ts
+                }
             )
             if len(sub_kline_data) == 0:
                 break
