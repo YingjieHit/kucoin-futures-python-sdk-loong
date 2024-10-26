e95389a992ec0e0d7cf759611dc83e982e43d388
YingjieHit <258349394@qq.com>
2024-10-26 12:08:50 +0800

    1

--- kucoin_futures/strategy/okx_base_cta.py
+++ kucoin_futures/strategy/okx_base_cta.py
@@ -74,9 +74,11 @@ class OkxBaseCta(object):
 
     async def _watch_okx_order_book5(self, symbol):
         while True:
-            order_book5 = await self._oxk_exchange.watch_order_book(symbol, params={'channel': 'books'})
+            order_book5 = await self._oxk_exchange.watch_order_book(
+                symbol,
+                # params={'channel': 'books'},
+            )
             print(order_book5)
-
 
     async def on_bar(self, bar):
         raise NotImplementedError("需要实现on_bar")
