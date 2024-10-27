5471aa1a48ad3d061476ad461986027b8e31e316
YingjieHit <258349394@qq.com>
2024-10-27 11:08:50 +0800

    111

--- kucoin_futures/strategy/okx_base_cta.py
+++ kucoin_futures/strategy/okx_base_cta.py
@@ -52,7 +52,7 @@ class OkxBaseCta(object):
 
     async def _fetch_position(self, symbol):
         position = await self._okx_exchange.fetch_position(symbol)
-        if position['contractSize'] == 0 and position['side'] is None:
+        if position is None or position['contractSize'] == 0 and position['side'] is None:
             return 0
         elif position['side'] == 'long':
             return position['contractSize']
